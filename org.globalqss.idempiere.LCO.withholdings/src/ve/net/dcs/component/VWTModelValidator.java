0c8a1b37efe672aabf0c1e7fe8469129f8f4251a
Ing. Victor Suarez <victor.suarez.is@gmail.com>
2017-08-18 15:54:15 -0400

    - Se corrige validacion de Numero de Control.   - Se valida cuando este activo el check "Is Control No Document".   - Si es Factura de Compras, solo valida que no este vacio el Numero de Control, si es de Ventas, verifica que tenga seleccionada una secuencia para el numero de Control.

--- org.globalqss.idempiere.LCO.withholdings/src/ve/net/dcs/component/VWTModelValidator.java
+++ org.globalqss.idempiere.LCO.withholdings/src/ve/net/dcs/component/VWTModelValidator.java
@@ -186,17 +186,17 @@ public class VWTModelValidator extends AbstractEventHandler {
 			MDocType docType = (MDocType) invoice.getC_DocType();
 
 			if (invoice.getReversal_ID() == 0)
-				if(MSysConfig.getValue("LVE_ValidateControlNumber", "Y", invoice.getAD_Client_ID(), invoice.getAD_Org_ID()).compareTo("Y")==0){
+				if(docType.get_ValueAsBoolean("isControlNoDocument")) {
 					if (invoice.isSOTrx()) {
 						String controlSequence = null;
 						if (invoice.get_Value("LVE_controlNumber") == null) {
-							if (docType.get_Value("LVE_ControlNoSequence_ID") == null && docType.get_ValueAsBoolean("isControlNoDocument")) {
+							if (docType.get_Value("LVE_ControlNoSequence_ID") == null) {
 								throw new AdempiereException(msgSeqNotFound);
 							}
 	
-							int docTypeID = docType.get_ValueAsInt("LVE_ControlNoSequence_ID");
-							System.out.println("Doctype ID =" + docTypeID);
-							MSequence seq = new MSequence(Env.getCtx(), docTypeID, po.get_TrxName());
+							int controlNoSequence_ID = docType.get_ValueAsInt("LVE_ControlNoSequence_ID");
+							System.out.println("Doctype ID =" + controlNoSequence_ID);
+							MSequence seq = new MSequence(Env.getCtx(), controlNoSequence_ID, po.get_TrxName());
 							controlSequence = MSequence.getDocumentNoFromSeq(seq, po.get_TrxName(), invoice);
 	
 							Query query = new Query(Env.getCtx(), MInvoice.Table_Name, where + "AND LVE_controlNumber=?", po.get_TrxName());
